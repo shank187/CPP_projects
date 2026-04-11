c1d4ee274d872f813a5eb634a8b336f2caee991e
shank187 <aelbour@e2r5p7.1337.ma>
2026-04-11 10:57:08 +0100

    adding my custom tests, and creating memeber function to add multiple element at once

--- cpp_08/ex01/Span.cpp
+++ cpp_08/ex01/Span.cpp
@@ -1,1 +1,70 @@
 #include "Span.hpp"
+#include <algorithm>
+#include <climits>
+
+const char * Span::SpanFullException::what() const throw()
+{
+    return "Span is full, you cant add more numbers!";
+}
+
+const char * Span::TooFewEelementsException::what() const throw()
+{
+    return "No enough numbers to Calculate Span!";
+}
+Span::Span():N(0){}
+
+Span::~Span(){}
+
+Span::Span(int n):N(n){}
+
+Span::Span(const Span & other)
+{
+    this->N = other.N;
+    this->v = other.v;
+}
+
+Span & Span::operator=(const Span & other)
+{
+    if(this != &other)
+    {
+        this->N = other.N;
+        this->v = other.v;
+    }
+    return (*this);
+}
+
+void Span::addNumber(int n)
+{
+    if(v.size() >= N)
+        throw SpanFullException();
+    v.push_back(n);
+}
+
+unsigned int Span::shortestSpan()
+{
+    if(v.size() < 2)
+        throw TooFewEelementsException();
+    std::vector<int> tmp = v;
+    std::sort(tmp.begin(), tmp.end());
+    unsigned int min_span = static_cast<unsigned int>(tmp[1]) - tmp[0];
+
+    for (std::size_t i = 2; i < tmp.size(); ++i)
+    {
+        unsigned int current_span = static_cast<unsigned int>(tmp[i]) - tmp[i - 1];
+        if(current_span < min_span)
+            min_span = current_span;
+        if(min_span == 0)
+            return 0;
+    }
+    return (min_span);
+}
+
+unsigned int Span::longestSpan()
+{
+    if(v.size() < 2)
+        throw TooFewEelementsException();
+
+    int min = *std::min_element(v.begin(), v.end());
+    int max = *std::max_element(v.begin(), v.end());
+    return(max - min);
+}
--- cpp_08/ex01/Span.hpp
+++ cpp_08/ex01/Span.hpp
@@ -1,17 +1,32 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <vector>
+
 class Span{
     private:
         unsigned int N;
-        // to fill
+        std::vector <int> v;
     public:
+        class SpanFullException: public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+        class TooFewEelementsException: public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
         Span();
         Span(int n);
         Span(const Span & other);
         ~Span();
         Span& operator=(const Span & other);
+        
         void addNumber(int n);
+        unsigned int shortestSpan();
+        unsigned int longestSpan();
 };
 
 #endif
