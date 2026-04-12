286fba131f79c4b70f545fa19c7fe3b86092a88a
shank187 <aymane@aymane.localdomain>
2026-04-12 07:35:47 +0100

    figuring it out

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -1,6 +1,21 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <stack>
+#include <deque>
+#include <iostream>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+
+    public:
+        typedef typename  std::stack<T>::container_type::iterator iterator;
+        typedef typename  std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename  std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename  std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+};
+
 
 #include "MutantStack.tpp"
 
