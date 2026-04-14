e6b330358004da3285a226a8f096c4aff029c461
shank187 <aymaneelbour33@gmail.com>
2026-04-14 12:31:01 +0100

    making parse line logique

--- cpp_09/ex00/BitcoinExchange.cpp
+++ cpp_09/ex00/BitcoinExchange.cpp
@@ -1,4 +1,8 @@
 #include "BitcoinExchange.hpp"
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
 #include <string>
 
 BitcoinExchange::BitcoinExchange(){}
@@ -18,3 +22,48 @@ BitcoinExchange & BitcoinExchange::operator=(const BitcoinExchange & other){
 
 BitcoinExchange::~BitcoinExchange(){}
 
+const char * BitcoinExchange::DataBaseException::what() const throw()
+{
+    return "An error occurred During opening/reading the DataBase.";
+}
+
+const char * BitcoinExchange::InputFileException::what() const throw()
+{
+    return "An Error occurred during Opening/Reading the Input File.";
+}
+
+bool BitcoinExchange::endwith(const std::string &str, const std::string &suffix)
+{
+    if(str.length() >= suffix.length())
+    {
+        return (0 == str.compare(str.length() - suffix.length(), suffix.length(), suffix));
+    }
+    return false;
+}
+
+void BitcoinExchange::loadDatabase(const std::string & path)
+{   try
+    {
+        std::ifstream db;
+        this->validatePath(path, db, false);
+        this->validateLines(db, false);
+    }
+    catch (const std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+}
+
+void BitcoinExchange::validatePath(const std::string & path, std::ifstream & to_save, bool is_input_file) const
+{
+    if(!is_input_file && !BitcoinExchange::endwith(path, ".csv"))
+        throw std::runtime_error("This program must use a database in csv format.");
+    to_save.open(path);
+    if (to_save.is_open())
+        throw DataBaseException();
+}
+
+void BitcoinExchange::validateLines(const std::ifstream & file, bool is_input_file)
+{
+
+}
--- cpp_09/ex00/BitcoinExchange.hpp
+++ cpp_09/ex00/BitcoinExchange.hpp
@@ -1,6 +1,8 @@
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
+#include <exception>
+#include <fstream>
 #include <map>
 #include <string>
 
@@ -9,7 +11,14 @@ class BitcoinExchange {
     private:
         std::map<std::string, float> Price_History;
     public:
-    
+        class DataBaseException: public std::exception{
+            public:
+                virtual const char * what() const throw();
+        };
+        class InputFileException: public std::exception{
+            public:
+                virtual const char * what() const throw();
+        };
         BitcoinExchange();
         BitcoinExchange(const std::string & DataBase);
         BitcoinExchange(const BitcoinExchange & other);
@@ -17,10 +26,13 @@ class BitcoinExchange {
         BitcoinExchange & operator=(const BitcoinExchange & other);
         void loadDatabase(const std::string & path);
         void exchangeBtc(const std::string & path, bool is_input_file);
-        void validatePath(const std::string &path) const;
-        void validateLine(const std::string & line, bool is_input_file) const;
+        void validatePath(const std::string &path, std::ifstream & to_save, bool is_input_file) const;
+        void validateLines(const std::ifstream & file, bool is_input_file);
         void parseDate(const std::string & field) const;
         void parsePrice(const std::string & field) const;
+
+        //helpers
+        static bool endwith(const std::string &str, const std::string &suffix);
 };
 
 
