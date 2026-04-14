d4b290eee8478cc067bdd05e6eb73ab8d020c9b6
shank187 <aymane@aymane.localdomain>
2026-04-14 08:47:48 +0100

    i ll tart work on parse

--- cpp_09/ex00/BitcoinExchange.hpp
+++ cpp_09/ex00/BitcoinExchange.hpp
@@ -2,7 +2,26 @@
 #define BITCOINEXCHANGE_HPP
 
 #include <map>
+#include <string>
 
+
+class BitcoinExchange {
+    private:
+        std::map<std::string, float> Price_History;
+    public:
+    
+        BitcoinExchange();
+        BitcoinExchange(const std::string & DataBase);
+        BitcoinExchange(const BitcoinExchange & other);
+        ~BitcoinExchange();
+        BitcoinExchange & operator=(const BitcoinExchange & other);
+        void loadDatabase(const std::string & path);
+        void exchangeBtc(const std::string & path, bool is_input_file);
+        void validatePath(const std::string &path) const;
+        void validateLine(const std::string & line, bool is_input_file) const;
+        void parseDate(const std::string & field) const;
+        void parsePrice(const std::string & field) const;
+};
 
 
 
