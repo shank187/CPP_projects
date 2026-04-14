8341d5d4e154d46caa725fa1004dd436a67a448f
shank187 <aymane@aymane.localdomain>
2026-04-14 10:29:36 +0100

    maKing ocf

--- cpp_09/ex00/BitcoinExchange.cpp
+++ cpp_09/ex00/BitcoinExchange.cpp
@@ -1,2 +1,20 @@
 #include "BitcoinExchange.hpp"
+#include <string>
+
+BitcoinExchange::BitcoinExchange(){}
+
+BitcoinExchange::BitcoinExchange(const std::string & DataBase)
+{
+    this->loadDatabase(DataBase);
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange & other) : Price_History(other.Price_History){}
+
+BitcoinExchange & BitcoinExchange::operator=(const BitcoinExchange & other){
+    if(this != &other)
+        this->Price_History = other.Price_History;
+    return *this;
+}
+
+BitcoinExchange::~BitcoinExchange(){}
 
