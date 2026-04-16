d9a1a43b7b5da4ca496c8739391172190b05e56d
shank187 <aymaneelbour33@gmail.com>
2026-04-16 18:10:53 +0100

    setting ex02

---
+++ cpp_09/ex02/PmergeMe.hpp
@@ -0,0 +1,16 @@
+#ifndef PMERGEME_hpp
+#define PMERGEME_hpp
+
+
+#include <deque>
+#include <vector>
+class PmergeMe{
+    private:
+        std::vector<unsigned int> vec;
+        std::deque<unsigned int> deq;
+    public:
+    
+};
+
+
+#endif
